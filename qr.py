8869d7390f7af3d4f676dcec8a36ad138c7d5549
Андрей Боев <a.boev.it@gmail.com>
2023-01-18 11:40:03 +0300

    Fix bugs 3 back_color dialogs (отмена при открытии)

--- qr.py
+++ qr.py
@@ -6,7 +6,6 @@ from qrcode.image.styledpil import StyledPilImage
 def generate_qr(data, module_driver, color_mask, image):
     qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H, border=3)
     qr.add_data(data)
-    qr.make(fit=False)
     img = qr.make_image(image_factory=StyledPilImage,
                         module_drawer=module_driver,
                         embeded_image_path=image,
